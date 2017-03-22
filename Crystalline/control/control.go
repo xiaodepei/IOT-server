e3b8eb6e4b7b4382ac23757dd70cfe6a4fee3a73
xiaodepei <990834049@qq.com>
2017-03-22 19:39:02 +0800

    此次更新了远程跨站控制的权限认证部分，与之前的增加/删除设备的协议统一

--- Crystalline/control/control.go
+++ Crystalline/control/control.go
@@ -2,6 +2,7 @@ package control
 
 import (
 	. "Crystalline/conf"
+	. "Crystalline/login"
 	. "Crystalline/tools"
 	. "Crystalline/udp"
 	"fmt"
@@ -15,35 +16,32 @@ import (
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
+	var flag string
 	var mesage string
 	var groupnum int64 //用来
 	r.ParseForm()      //解析参数
-	for k, v := range r.Form {
-		key_buf := k
-		passwd_buf := strings.Join(v, "")
+	name := r.PostFormValue("name")
+	key := r.PostFormValue("key")
+	flag = Getuser(name, key)
+	if "no user" == flag {
+		fmt.Fprintf(w, " err ")
+	} else if "pass" == flag {
+		mesage = r.PostFormValue("groupnum")
+		a := time.Now().UnixNano()
+		randkey := strconv.FormatInt(a, 10)
+		member := Client0.ZRangeByScore("2", redis.ZRangeBy{mesage, mesage, 0, 0}).Val() //拿出指令中需要的全部设备ID，后续版本加入同一条指令查询多组的功能
 
-		if Name == key_buf {
-			//			println("name confirmed!")
-			mesage = passwd_buf
+		for num, i := range member { //num是通过数组下标来计数member的，因为从0开始所以需要+1，i是每个deviceid
+			Client0.ZAdd(randkey, redis.Z{0, i})
+			groupnum = int64(num)
 		}
+		group_with_offline_num := Client0.ZInterStore(randkey, redis.ZStore{Weight, "sum"}, randkey, "Offline").Val() //将求交集之后的可用device储存在randkey命名的表中，且同时得到可用设备数量
+		groupnum = groupnum + 1 - group_with_offline_num                                                              //因为是下标，所以需要+1，然后再减去离线的设备
+		Client0.ZAdd("0", redis.Z{float64(groupnum), randkey})                                                        //将randkey为srt写入db0中，记录对应randkey的次数，接受函数需要使用到
+		go Findgroup(mesage, randkey)                                                                                 //放在这里比放在上面快多了，groupnum其实和message一样，就是查询一下当前分组有多少个设备
+		fmt.Fprintf(w, randkey)                                                                                       //输出到客户端
 
 	}
-
-	a := time.Now().UnixNano()
-	randkey := strconv.FormatInt(a, 10)
-	member := Client0.ZRangeByScore("2", redis.ZRangeBy{mesage, mesage, 0, 0}).Val() //拿出指令中需要的全部设备ID，后续版本加入同一条指令查询多组的功能
-
-	for num, i := range member { //num是通过数组下标来计数member的，因为从0开始所以需要+1，i是每个deviceid
-		Client0.ZAdd(randkey, redis.Z{0, i})
-		groupnum = int64(num)
-	}
-	group_with_offline_num := Client0.ZInterStore(randkey, redis.ZStore{Weight, "sum"}, randkey, "Offline").Val() //将求交集之后的可用device储存在randkey命名的表中，且同时得到可用设备数量
-	groupnum = groupnum + 1 - group_with_offline_num                                                              //因为是下标，所以需要+1，然后再减去离线的设备
-
-	Client0.ZAdd("0", redis.Z{float64(groupnum), randkey}) //将randkey为srt写入db0中，记录对应randkey的次数，接受函数需要使用到
-	go Findgroup(mesage, randkey)                          //放在这里比放在上面快多了，groupnum其实和message一样，就是查询一下当前分组有多少个设备
-
-	fmt.Fprintf(w, randkey) //输出到客户端
 
 }
 func Watch_dog() {
