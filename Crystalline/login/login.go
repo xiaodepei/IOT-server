4d1ae7444f5e664f9805be084c9cd2f7b388275d
xiaodepei <990834049@qq.com>
2017-04-10 09:53:09 +0800

    修复了http验证模块，确保部署在同一server下也可以正确判断目标服务器在线情况

--- Crystalline/login/login.go
+++ Crystalline/login/login.go
@@ -27,14 +27,28 @@ func Getuser(name string, pwd string) (state string) {
 //验证服务器是否处于联通状态
 //联通返回：online
 //出现问题：offline
+//func Getwebalive(URL string) (state string) {
+//	resp, err := http.Get(URL)
+//	if err != nil {
+//		fmt.Println("WEBserver——offline")
+//		state = "offline"
+//	} else {
+//		resp.Body.Close()
+//		state = "online"
+//	}
+//	return state
+//}
+
 func Getwebalive(URL string) (state string) {
-	resp, err := http.Get(URL)
-	if err != nil {
-		fmt.Println("WEB端服务离线：", err)
-		state = "offline"
-	} else {
-		resp.Body.Close()
+	resp, _ := http.Get(URL)
+	a := resp.StatusCode
+	//	defer resp.Body.Close()
+	if a == 200 {
 		state = "online"
+		resp.Body.Close()
+	} else {
+		fmt.Println("WEBserver——offline")
+		state = "offline"
 	}
 	return state
 }
